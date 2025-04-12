2aec700bbc5f5ef3cbc3f491044c1d7426a27834
iaris <108977117+iarabueno@users.noreply.github.com>
2025-04-12 19:10:46 -0300

    Update practica2.sql

--- practica2.sql
+++ practica2.sql
@@ -16,4 +16,42 @@ begin
     dbms_output.put_line('Hola, soy ' || v_username);
      dbms_output.put_line'Hoy es: ' || v_fecha);
 END;
+
+/*
+2. Crear un bloque Pl/Sql para consultar el salario de un empleado dado:
+Ingresar el id del empleado usando una variable de sustitución
+Desplegar por pantalla el siguiente texto:
+First_name, Last_name  tiene un salario de Salary pesos.
 */
+
+-- select employee_id 
+-- from employee
+-- order by employee_id;
+
+
+declare
+
+v_id_empleado employee.employee_id%type := 7565;  --&id_del_empleado;
+v_first_name_empleado employee.first_name%type;
+v_last_name_empleado employee.last_name%type;
+v_salary_empleado employee.salary%type;
+
+begin
+
+select first_name, last_name, salary
+into v_first_name_empleado, v_last_name_empleado, v_salary_empleado
+from employee
+where employee_id = v_id_empleado;
+
+dbms_output.put_line('nombre: ' || v_first_name_empleado || ' apellido: ' || v_last_name_empleado || ' salario: ' || v_salary_empleado);
+
+exception
+
+when NO_DATA_FOUND then
+dbms_output.put_line('no se encontró el empleado');
+
+when OTHERS then
+dbms_output.put_line('ocurrió un error');
+end;
+
+
